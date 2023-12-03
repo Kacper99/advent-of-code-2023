31dac9a8d2f08b08284b3da1276ff1bb4bc15091
Kacper Martela <kacper99@hotmail.co.uk>
2023-12-03 14:26:35 +0000

    refactor: add a stram option

--- day1/src/main/java/com/kacpermartela/aoc/Trebuchet.java
+++ day1/src/main/java/com/kacpermartela/aoc/Trebuchet.java
@@ -3,19 +3,27 @@ package com.kacpermartela.aoc;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 public class Trebuchet {
 
     int calibrate(List<String> lines) {
         List<Integer> nums = new ArrayList<>();
         for (final var line : lines) {
-            String digits = line.replaceAll("\\D+", "");
-            String firstDigit = String.valueOf(digits.charAt(0));
-            String lastDigit = String.valueOf(digits.charAt(digits.length() - 1));
-            int number = Integer.parseInt(firstDigit + lastDigit);
-            nums.add(number);
+            nums.add(getNumber(line));
         }
         return nums.stream().reduce(0, Integer::sum);
+    }
+
+    private static int getNumber(String line) {
+        String digits = line.replaceAll("\\D+", "");
+        String firstDigit = String.valueOf(digits.charAt(0));
+        String lastDigit = String.valueOf(digits.charAt(digits.length() - 1));
+        return Integer.parseInt(firstDigit + lastDigit);
+    }
+
+    int calibrate(Stream<String> lines) {
+        return lines.map(Trebuchet::getNumber).reduce(0, Integer::sum);
     }
 
     final Map<String, String> wordToNumber = Map.of(
@@ -32,15 +40,22 @@ public class Trebuchet {
     int calibrate2(List<String> lines) {
         List<Integer> nums = new ArrayList<>();
         for (var line : lines) {
-            for (final var entry : wordToNumber.entrySet()) {
-                line = line.replaceAll(entry.getKey(), entry.getValue());
-            }
-            String digits = line.replaceAll("\\D+", "");
-            String firstDigit = String.valueOf(digits.charAt(0));
-            String lastDigit = String.valueOf(digits.charAt(digits.length() - 1));
-            int number = Integer.parseInt(firstDigit + lastDigit);
-            nums.add(number);
+            line = replaceWordsWithNumbers(line);
+            nums.add(getNumber(line));
         }
         return nums.stream().reduce(0, Integer::sum);
+    }
+
+    int calibrate2(Stream<String> lines) {
+        return lines.map(this::replaceWordsWithNumbers)
+                .map(Trebuchet::getNumber)
+                .reduce(0, Integer::sum);
+    }
+
+    private String replaceWordsWithNumbers(String line) {
+        for (final var entry : wordToNumber.entrySet()) {
+            line = line.replaceAll(entry.getKey(), entry.getValue());
+        }
+        return line;
     }
 }
--- day1/src/test/java/com/kacpermartela/aoc/TrebuchetTest.java
+++ day1/src/test/java/com/kacpermartela/aoc/TrebuchetTest.java
@@ -1,5 +1,7 @@
 package com.kacpermartela.aoc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,18 +14,32 @@ class TrebuchetTest {
     void part1() throws IOException {
         final var lines = Files.readAllLines(Path.of("src/test/resources/input.txt"));
         final var trebuchet = new Trebuchet();
-        System.out.println(trebuchet.calibrate(lines));
+        assertThat(trebuchet.calibrate(lines)).isEqualTo(54990);
+    }
+
+    @Test
+    void part1_stream() throws IOException {
+        final var lines = Files.lines(Path.of("src/test/resources/input.txt"));
+        final var trebuchet = new Trebuchet();
+        assertThat(trebuchet.calibrate(lines)).isEqualTo(54990);
     }
 
     @Test
     void part2() throws IOException {
         final var lines = Files.readAllLines(Path.of("src/test/resources/input.txt"));
         final var trebuchet = new Trebuchet();
-        System.out.println(trebuchet.calibrate2(lines));
+        assertThat(trebuchet.calibrate2(lines)).isEqualTo(54473);
     }
 
     @Test
-    void part2small() throws IOException {
+    void part2_stream() throws IOException {
+        final var lines = Files.lines(Path.of("src/test/resources/input.txt"));
+        final var trebuchet = new Trebuchet();
+        assertThat(trebuchet.calibrate2(lines)).isEqualTo(54473);
+    }
+
+    @Test
+    void part2small() {
         final var lines = List.of(
                 "two1nine",
                 "eightwothree",
