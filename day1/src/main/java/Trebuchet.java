3c026406d4256241ee84a312714f112acfa7f060
Kacper Martela <kacper99@hotmail.co.uk>
2023-12-03 14:15:28 +0000

    feat: day 1 part 2

--- day1/src/main/java/Trebuchet.java
+++ day1/src/main/java/Trebuchet.java
@@ -1,11 +1,39 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Trebuchet {
 
     int calibrate(List<String> lines) {
         List<Integer> nums = new ArrayList<>();
         for (final var line : lines) {
+            String digits = line.replaceAll("\\D+", "");
+            String firstDigit = String.valueOf(digits.charAt(0));
+            String lastDigit = String.valueOf(digits.charAt(digits.length() - 1));
+            int number = Integer.parseInt(firstDigit + lastDigit);
+            nums.add(number);
+        }
+        return nums.stream().reduce(0, Integer::sum);
+    }
+
+    final Map<String, String> wordToNumber = Map.of(
+            "one", "one1one",
+            "two", "two2two",
+            "three", "three3three",
+            "four", "four4four",
+            "five", "five5five",
+            "six", "six6six",
+            "seven", "seven7seven",
+            "eight", "eight8eight",
+            "nine", "nine9nine"
+    );
+
+    int calibrate2(List<String> lines) {
+        List<Integer> nums = new ArrayList<>();
+        for (var line : lines) {
+            for (final var entry : wordToNumber.entrySet()) {
+                line = line.replaceAll(entry.getKey(), entry.getValue());
+            }
             String digits = line.replaceAll("\\D+", "");
             String firstDigit = String.valueOf(digits.charAt(0));
             String lastDigit = String.valueOf(digits.charAt(digits.length() - 1));
--- day1/src/test/java/TrebuchetTest.java
+++ day1/src/test/java/TrebuchetTest.java
@@ -3,7 +3,9 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TrebuchetTest {
@@ -13,6 +15,28 @@ class TrebuchetTest {
         final var lines = Files.readAllLines(Path.of("src/test/resources/input.txt"));
         final var trebuchet = new Trebuchet();
         System.out.println(trebuchet.calibrate(lines));
+    }
+
+    @Test
+    void part2() throws IOException {
+        final var lines = Files.readAllLines(Path.of("src/test/resources/input.txt"));
+        final var trebuchet = new Trebuchet();
+        System.out.println(trebuchet.calibrate2(lines));
+    }
+
+    @Test
+    void part2small() throws IOException {
+        final var lines = List.of(
+                "two1nine",
+                "eightwothree",
+                "abcone2threexyz",
+                "xtwone3four",
+                "4nineeightseven2",
+                "zoneight234",
+                "7pqrstsixteen"
+        );
+        final var trebuchet = new Trebuchet();
+        System.out.println(trebuchet.calibrate2(lines));
     }
 
 }
