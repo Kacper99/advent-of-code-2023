10949ae70ed89baa05c87377765513001c8385ec
Kacper Martela <kacper99@hotmail.co.uk>
2023-12-04 21:34:18 +0000

    feat: day 4 part 1 and 2

--- day4/src/main/java/com/kacpermartela/aoc/Scratchcards.java
+++ day4/src/main/java/com/kacpermartela/aoc/Scratchcards.java
@@ -1,5 +1,6 @@
 package com.kacpermartela.aoc;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -9,21 +10,51 @@ public class Scratchcards {
 
 
     int points(List<String> input) {
+        int totalScore = 0;
         for (var line: input) {
             var scratchcard = Scratchcard.parse(line);
-
-
+            int count = 0;
+            for (var number: scratchcard.cardNumbers()) {
+                if (scratchcard.winningNumbers().contains(number)) {
+                    count++;
+                }
+            }
+            if (count > 0) {
+                totalScore += (int) Math.pow(2, count - 1);
+            }
+            System.out.printf("Score for card %d is %d, totalScore is %d%n", scratchcard.cardNumber(), 2 ^ (count - 1), totalScore);
         }
-        return 0;
+        return totalScore;
+    }
+
+    int points2(List<String> input) {
+        int[] counts = new int[input.size()];
+        for (int i = 0; i < input.size(); i++) {
+            var line = input.get(i);
+            var scratchcard = Scratchcard.parse(line);
+            counts[i]++;
+            int count = 0;
+            for (var number : scratchcard.cardNumbers()) {
+                if (scratchcard.winningNumbers().contains(number)) {
+                    count++;
+                }
+            }
+            for (int j = 0; j < count; j++) {
+                counts[i + j + 1] += counts[i];
+            }
+        }
+        System.out.println(Arrays.toString(counts));
+        return Arrays.stream(counts).sum();
     }
 
     record Scratchcard(int cardNumber, List<Integer> winningNumbers, List<Integer> cardNumbers) {
         static Scratchcard parse(String line) {
+            System.out.println("Parsing line: " + line);
             String[] nameAndNumbers = line.split(":");
 
             //Extract name
             String name = nameAndNumbers[0].replace(":", "").strip();
-            int cardNumber = Integer.parseInt(name.split(" ")[1]);
+            int cardNumber = Integer.parseInt(name.split("\\s+")[1]);
 
             //Extract winning numbers
             String[] winningAndActualNumbers = nameAndNumbers[1].split("\\|");
--- day4/src/test/java/com/kacpermartela/aoc/ScratchcardsTest.java
+++ day4/src/test/java/com/kacpermartela/aoc/ScratchcardsTest.java
@@ -2,6 +2,10 @@ package com.kacpermartela.aoc;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +20,55 @@ class ScratchcardsTest {
         assertThat(scratchcard.cardNumber()).isEqualTo(1);
         assertThat(scratchcard.winningNumbers()).containsExactly(41, 48, 83, 86, 17);
         assertThat(scratchcard.cardNumbers()).containsExactly(83, 86,  6, 31, 17,  9, 48, 53);
+    }
+
+    @Test
+    void smallInputTest() {
+        var input = """
+                Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+                Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+                """.lines().toList();
+
+        Scratchcards scratchcards = new Scratchcards();
+        assertThat(scratchcards.points(input)).isEqualTo(13);
+    }
+
+    @Test
+    void part1() throws IOException {
+        var inputLines = Files.readAllLines(Path.of("src/test/resources/input.txt"));
+
+        var scratchcards = new Scratchcards();
+
+        assertThat(scratchcards.points(inputLines)).isEqualTo(0);
+    }
+
+    @Test
+    void part2SmallInput() {
+        var input = """
+                Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+                Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+                Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+                Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+                Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+                Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+                """.lines().toList();
+
+        Scratchcards scratchcards = new Scratchcards();
+
+        assertThat(scratchcards.points2(input)).isEqualTo(30);
+    }
+
+    @Test
+    void part2() throws IOException {
+        var inputLines = Files.readAllLines(Path.of("src/test/resources/input.txt"));
+
+        Scratchcards scratchcards = new Scratchcards();
+
+        assertThat(scratchcards.points2(inputLines)).isEqualTo(5571760);
     }
 
 }
